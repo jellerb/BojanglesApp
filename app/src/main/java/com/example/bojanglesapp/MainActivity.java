a4966bee72b87023716ed7fdf12a0fd9cd85c82f
Stephanie <slee185@uncc.edu>
2023-03-21 10:56:31 -0400

    start layout for login

--- app/src/main/java/com/example/bojanglesapp/MainActivity.java
+++ app/src/main/java/com/example/bojanglesapp/MainActivity.java
@@ -5,12 +5,16 @@
 
 package com.example.bojanglesapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.auth.User;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +26,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentUser = getIntent().getParcelableExtra("user");
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.rootView, new LoginFragment())
+                .commit();
     }
+
+//    @Override
+//    public void logout() {
+//        Map<String, Object> data = new HashMap<>();
+//        data.put("online", false);
+//
+//        // To keep things responsive, we intentionally ignore the response.
+//        firebaseFirestore
+//                .collection("Users")
+//                .document(currentUser.getUserId())
+//                .update(data);
+//
+//        Intent intent = new Intent(MainActivity.this, AuthActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
+
+
 }
